0eae9e1afdfe541dad327ee16d3983884dad91eb
John Vascsinec <pensteeler@gmail.com>
2017-10-19 18:08:23 +0000

    automatically backed up by learn

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -31,7 +31,7 @@ class PoemWriter extends React.Component {
     // Third line has five words
     const poemLines = poemText.split(/\r\n|\r|\n/);
 
-    //console.log( "Line1: " + poemLines[0] + "Length: " + poemLines[0].trim().split(" ").length);
+    console.log( "Line1: " + poemLines[0] + "Length: " + poemLines[0].trim().split(" ").length);
     //console.log( "Line2: " + poemLines[1] + "Length: " + poemLines[1].trim().split(" ").length);
     //console.log( "Line3: " + poemLines[2] + "Length: " + poemLines[2].trim().split(" ").length);
 
